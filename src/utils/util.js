f928bea3e1bbba74d9e183d2c5b0b3896c2c7ca3
2ue <jie746635835@163.com>
2017-09-13 17:08:28 +0800

    update readme.md

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,24 +1,26 @@
-
 export default {
-    tryType: para => {
-        const type = typeof para;
-        if(type === 'number' && isNaN(para)) return 'NaN';
-        if(type !== 'object') return type;
-        return Object.prototype.toString.call(para).replace(/[\[\]]/g,'').split(' ')[1].toLowerCase();
-    },
+  tryType: para => {
+    const type = typeof para;
+    if (type === "number" && isNaN(para)) return "NaN";
+    if (type !== "object") return type;
+    return Object.prototype.toString
+      .call(para)
+      .replace(/[\[\]]/g, "")
+      .split(" ")[1]
+      .toLowerCase();
+  },
   isNumber: num => {
-    if(window.isNumber) return window.isNumber(num);
-    if(Number.isNumber) return Number.isNumber(num);
-    return typeof num !== 'undefined' && !isNaN(num);
+    if (window.isNumber) return window.isNumber(num);
+    if (Number.isNumber) return Number.isNumber(num);
+    return typeof num !== "undefined" && !isNaN(num);
   },
 
-
   //深层取值
-  getItem: (props, target) => {
-      if(!target) target = this;
-    if(Array.reduce) return props.reduce((pre, nxt) => (pre && pre[nxt]) ? pre[nxt] : null, target);
-    // if(Array.forEach) return props.forEach((pre, nxt) => (pre && pre[nxt]) ? pre[nxt] : null, target);
-
+  getDeepData: (props, target) => {
+    if (!target) target = this;
+    return props.reduce(
+      (pre, nxt) => (pre && pre[nxt] ? pre[nxt] : null),
+      target
+    );
   }
-}
-
+};
