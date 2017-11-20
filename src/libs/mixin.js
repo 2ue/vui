53a911891921d8567ec3dde95d6d089f6a966011
2ue <jie746635835@163.com>
2017-11-20 17:25:20 +0800

    add common method

--- src/libs/mixin.js
+++ src/libs/mixin.js
@@ -12,6 +12,19 @@ export default {
         getValueFromDeepData: (props, target) => {
             if (!props || !target) return undefined;
             return props.reduce((pre, nxt) => (typeof pre === 'undefined' || typeof pre[nxt] === 'undefined' ? undefined : pre[nxt]), target);
+        },
+        decodeStr: (str, type) => {//type: html, html2
+            function htmlDecode(str) {
+                let e = document.createElement('div'), _html = '';
+                e.innerHTML = str;
+                for (let i = 0; i < e.childNodes.length; i++) {
+                    _html += e.childNodes[i].nodeValue;
+                };
+                return _html;
+            };
+            if (type === 'html') return !str ? '' : htmlDecode(str);
+            if (type === 'html2') return !str ? '' : htmlDecode(htmlDecode(str));
+            return !str ? '' : decodeURIComponent(decodeURI(htmlDecode(htmlDecode(str))));
         }
     }
 }
