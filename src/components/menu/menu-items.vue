6b113d6a127039a66ba8c81e87e0ce37ac364562
2ue <jie7466358356@153.com>
2017-11-28 20:13:18 +0800

    update

--- src/components/menu/menu-items.vue
+++ src/components/menu/menu-items.vue
@@ -1,11 +1,11 @@
 <template>
     <transition name="menu-items">
         <ul class="vui-menu-items f-disselected">
-            <li class="vui-menu-item" v-for="(item,index) in selfMenuData" :class="{'vui-actived': item._actived || (item[slefMapTable.href] && $route.fullPath === item[slefMapTable.href])}">
-                <p v-if="item[slefMapTable.href]" @click="showChildren(index)" :style="getStyleObject(item)">
-                    <router-link :to="item[slefMapTable.href]">{{ item[slefMapTable.name] }}</router-link>
+            <li class="vui-menu-item" v-for="(item,index) in selfMenuData" :class="getActivedClass(item)">
+                <p v-if="item[slefMapTable.href]" >
+                    <router-link :to="item[slefMapTable.href]" @click.stop="showChildren(index)" :style="getStyleObject(item)">{{ item[slefMapTable.name] }}</router-link>
                 </p>
-                <p v-else @click="showChildren(index)" :style="getStyleObject(item)">{{ item[slefMapTable.name] }}</p>
+                <p v-else class="vui-menu-custom" @click="showChildren(index)" :style="getStyleObject(item)">{{ item[slefMapTable.name] }}</p>
                 <menuItem v-if="item._showChildren && !!item.children && item.children.length > 0" :menuData="item.children" :showOne="showOne" :mapTable="slefMapTable"></menuItem>
             </li>
         </ul>
@@ -35,6 +35,10 @@
         methods: {
             getStyleObject(item) {
                 return { padding: '8px ' + (item._deep * 15 + 10) + 'px' }
+            },
+            getActivedClass(item){
+                const selfHref= this.slefMapTable.href;
+                return {'vui-actived': item._actived || (item[selfHref] && this.$route.fullPath === item[selfHref])}
             },
             showChildren(index) {
                 let items = this.selfMenuData[index], itemChildrens = items.children, oldStatus = items._showChildren;
