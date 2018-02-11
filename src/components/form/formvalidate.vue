997707bf9f1c1db11f634ac09833c434d3a880a1
2ue <jie746635835@163.com>
2018-02-11 11:22:24 +0800

    merge

---
+++ src/components/form/formvalidate.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="form">
+        <button v-if="hideBtn" @click="submit">提交</button>
+        <solt></solt>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        data() {
+            return {}
+        },
+        props:{
+            hideBtn: {
+                type: Boolean,
+                default: false
+            },
+            formData:{
+                type: Object
+            },
+            validateRules:{
+                type: Object
+            }
+        },
+        computed:{
+            selfFormData(){
+                return this.formData;
+            },
+            selfValidateRules(){
+                return this.validateRules;
+            }
+        },
+        methods:{
+            submit(){
+
+            },
+            validateForm(){
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
